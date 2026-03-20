759035158737dabf927dd868abbae5029ee7d88b
Gerjinn C. Yallung <gerjinn.yallung@neu.edu.ph>
2026-03-20 11:52:20 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-9276369069-b05fa",
+  "appId": "1:498977455043:web:f4b8240ae17237f04c5b76",
+  "apiKey": "AIzaSyCEJwWaqtBngXyuIh9QQzlar0C_QkNZBj8",
+  "authDomain": "studio-9276369069-b05fa.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "498977455043"
+};
